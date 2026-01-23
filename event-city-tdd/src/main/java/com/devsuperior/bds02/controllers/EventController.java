cbeb0cdee5e21a05856e1000144a661bb3518d66
mateusribeirocampos <mateusrcampos@gmail.com>
2026-01-23 10:21:57 -0300

    Chore: All test done

--- event-city-tdd/src/main/java/com/devsuperior/bds02/controllers/EventController.java
+++ event-city-tdd/src/main/java/com/devsuperior/bds02/controllers/EventController.java
@@ -1,4 +1,21 @@
 package com.devsuperior.bds02.controllers;
 
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.services.EventServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/events")
 public class EventController {
+
+    @Autowired
+    private EventServices services;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto) {
+        dto = services.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
 }
--- event-city-tdd/src/main/java/com/devsuperior/bds02/services/EventServices.java
+++ event-city-tdd/src/main/java/com/devsuperior/bds02/services/EventServices.java
@@ -1,10 +1,14 @@
 package com.devsuperior.bds02.services;
 
 import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.entities.Event;
 import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.excpetions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EventServices {
@@ -12,10 +16,15 @@ public class EventServices {
     @Autowired
     private EventRepository repository;
 
+    @Transactional
     public EventDTO update(Long id, EventDTO dto) {
         try {
             Event entity = repository.getReferenceById(id);
             copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+            return new EventDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("id not found " + id);
         }
     }
 
@@ -23,5 +32,6 @@ public class EventServices {
         entity.setName(dto.getName());
         entity.setDate(dto.getDate());
         entity.setUrl(dto.getUrl());
+        entity.setCity(new City(dto.getCityId(), dto.getName()));
     }
 }
