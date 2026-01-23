dc09c7907252ae52470ebe838712a572690c62ae
mateusribeirocampos <mateusrcampos@gmail.com>
2026-01-23 14:59:34 -0300

    Fix: method copyDtoToEntity with correct cityId implemented

--- event-city-tdd/src/main/java/com/devsuperior/bds02/services/EventServices.java
+++ event-city-tdd/src/main/java/com/devsuperior/bds02/services/EventServices.java
@@ -3,6 +3,7 @@ package com.devsuperior.bds02.services;
 import com.devsuperior.bds02.dto.EventDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.CityRepository;
 import com.devsuperior.bds02.repositories.EventRepository;
 import com.devsuperior.bds02.services.excpetions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
@@ -15,6 +16,9 @@ public class EventServices {
 
     @Autowired
     private EventRepository repository;
+
+    @Autowired
+    private CityRepository cityRepository;
 
     @Transactional
     public EventDTO update(Long id, EventDTO dto) {
@@ -32,6 +36,8 @@ public class EventServices {
         entity.setName(dto.getName());
         entity.setDate(dto.getDate());
         entity.setUrl(dto.getUrl());
-        entity.setCity(new City(dto.getCityId(), dto.getName()));
+
+        City city = cityRepository.getReferenceById(dto.getCityId());
+        entity.setCity(city);
     }
 }
