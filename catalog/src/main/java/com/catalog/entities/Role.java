a0f1589b166802e07d6975f6bd7f7d05a8976ba3
mateusribeirocampos <mateusrcampos@gmail.com>
2026-01-23 17:24:36 -0300

    Chore: entity Role and User created

--- catalog/src/main/java/com/catalog/entities/Role.java
+++ catalog/src/main/java/com/catalog/entities/Role.java
@@ -1,11 +1,22 @@
 package com.catalog.entities;
 
-import java.util.Objects;
+import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_role")
 public class Role {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String authority;
+
+    //@ManyToMany(mappedBy = "roles")
+    //private Set<User> users = new HashSet<>();
 
     public Role() {
     }
