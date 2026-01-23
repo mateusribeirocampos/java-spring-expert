786277b3d155f6440b017340628ba63aab7e5579
mateusribeirocampos <mateusrcampos@gmail.com>
2026-01-23 08:27:32 -0300

    Chore: First test to findAll cities by name concluded

--- event-city-tdd/src/main/java/com/devsuperior/bds02/services/CityService.java
+++ event-city-tdd/src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -1,7 +1,24 @@
 package com.devsuperior.bds02.services;
 
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    public List<CityDTO> findAll() {
+        List<City> listEntity = repository.findAll(Sort.by("name"));
+        return listEntity.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+    }
+
 }
