c7ad6f152938bc914df8b8683c3db8731bb09312
mateusribeirocampos <mateusrcampos@gmail.com>
2026-01-23 16:41:11 -0300

    Chore: entity Role and User created

---
+++ dscatalog-cap3-3.4.4-ssToWs/src/main/java/com/devsuperior/dscatalog/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.devsuperior.dscatalog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable()); // Desativa CSRF
+		http.headers(headers -> headers.frameOptions(frame -> frame.disable())); // Permite o H2 Console
+
+		return http.build();
+	}
+}
