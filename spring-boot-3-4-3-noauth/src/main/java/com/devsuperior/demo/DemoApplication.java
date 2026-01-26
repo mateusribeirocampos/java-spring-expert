760b6f614d85dc40ab7411b3fec541b615073d59
mateusribeirocampos <mateusrcampos@gmail.com>
2026-01-26 16:57:01 -0300

    Chore: Bcryptpassword implemented

--- spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,31 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
+
+//	@Autowired
+//	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+//	@Override
+//	public void run(String... args) throws Exception {
+//		System.out.println("ENCODE: " + passwordEncoder.encode("123456"));
+//
+//		boolean result = passwordEncoder.matches("123456", "$2a$10$JsmUx4yvS64wv15eBU.7jO7iA4d7COPBv0YLEesh364GtZELN/or6");
+//
+//		if (result) {
+//			System.out.println("The password is matches");
+//		} else {
+//			System.out.println("The password is not matches");
+//		}
+//	}
 }
--- spring-boot-3-4-3-noauth/src/main/resources/import.sql
+++ spring-boot-3-4-3-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$JsmUx4yvS64wv15eBU.7jO7iA4d7COPBv0YLEesh364GtZELN/or6');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$JsmUx4yvS64wv15eBU.7jO7iA4d7COPBv0YLEesh364GtZELN/or6');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
