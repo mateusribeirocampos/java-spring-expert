6ace39c6dec70e322051bea45dc9b723c85224a0
mateusribeirocampos <mateusrcampos@gmail.com>
2026-01-29 17:45:36 -0300

    Chore: User and Role entities implemented

--- cap3/desafio-empregados-cap3/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ cap3/desafio-empregados-cap3/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.devsuperior.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,7 +12,8 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable());
+		http.csrf(AbstractHttpConfigurer::disable);
+		http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
 		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 		return http.build();
 	}
