edbaac4a6e7be3235477bbe118489558ee0efcc8
mateusribeirocampos <mateusrcampos@gmail.com>
2026-02-02 15:29:56 -0300

    Chore: layers implemented to City and Event and tested

---
+++ cap3/desafio-validacao-seguranca/src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.bds04.services;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    public List<CityDTO> findAll() {
+        List<City> entityList = repository.findAll();
+        return entityList.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+    }
+}
---
+++ cap3/desafio-validacao-seguranca/src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -0,0 +1,21 @@
+package com.devsuperior.bds04.services;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    public Page<EventDTO> findAll(Pageable pageable) {
+        Page<Event> entityList = repository.findAll(pageable);
+        return entityList.map(x -> new EventDTO(x));
+    }
+}
