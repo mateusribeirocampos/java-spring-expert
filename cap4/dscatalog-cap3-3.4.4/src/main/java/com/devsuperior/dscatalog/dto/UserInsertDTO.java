b7f965f863bbeed9424602b2359217ed7d07b041
mateusribeirocampos <mateusrcampos@gmail.com>
2026-02-05 10:05:17 -0300

    Chore: signup to test implemented - service layer a new logic to get role when creted new user

--- cap4/dscatalog-cap3-3.4.4/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ cap4/dscatalog-cap3-3.4.4/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,10 +1,16 @@
 package com.devsuperior.dscatalog.dto;
 
 import com.devsuperior.dscatalog.services.validation.UserInsertValid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
+	@NotBlank(message = "Campo obrigatório")
+	// @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$")
+	@Size(min = 8, message = "Deve ter mínimo 8 caracteres")
 	private String password;
 
 	UserInsertDTO() {
