0f4ffd380dd1dc2a607e44554ca43132ac0d3845
mateusribeirocampos <mateusrcampos@gmail.com>
2026-02-10 17:39:24 -0300

    Chore: Util classes to simplify test to user authenticated

---
+++ cap5/dscommerce-cap05/src/main/java/com/devsuperior/dscommerce/util/CustomUserUtil.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dscommerce.util;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+public class CustomUserUtil {
+
+    public String getLoggedUserName() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+        return jwtPrincipal.getClaim("username");
+    }
+}
