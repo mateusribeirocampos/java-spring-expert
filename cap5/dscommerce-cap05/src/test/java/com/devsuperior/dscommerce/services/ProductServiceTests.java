6059ec8aa00210e03901e6450debf4821a489e2f
mateusribeirocampos <mateusrcampos@gmail.com>
2026-02-10 14:43:52 -0300

    Chore: method findAll test implemented to ProductService, all passed

--- cap5/dscommerce-cap05/src/test/java/com/devsuperior/dscommerce/services/ProductServiceTests.java
+++ cap5/dscommerce-cap05/src/test/java/com/devsuperior/dscommerce/services/ProductServiceTests.java
@@ -1,19 +1,29 @@
 package com.devsuperior.dscommerce.services;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.dto.ProductMinDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.dscommerce.tests.ProductFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
 import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -27,6 +37,7 @@ public class ProductServiceTests {
     private long existingProductId, nonExistingProductId;
     private String productName;
     private Product product;
+    private PageImpl<Product> page;
 
     @BeforeEach
     void setUp() throws Exception {
@@ -37,9 +48,14 @@ public class ProductServiceTests {
 
         product = ProductFactory.createProduct(productName);
 
+        page = new PageImpl<>(List.of(product));
+
         // findById
         Mockito.when(productRepository.findById(existingProductId)).thenReturn(Optional.of(product));
-        Mockito
+        Mockito.when(productRepository.findById(nonExistingProductId)).thenReturn(Optional.empty());
+
+        // findAll
+        Mockito.when(productRepository.searchByName(any(), any(Pageable.class))).thenReturn(page);
     }
 
     @Test
@@ -52,5 +68,20 @@ public class ProductServiceTests {
     }
 
     @Test
-    public
+    public void findByIdShouldReturnResourceNotFoundExceptionWhenIdDoesNotExists() {
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            productService.findById(nonExistingProductId);
+        });
+    }
+
+    @Test
+    public void findAllShouldReturnPagedProductMinDTO() {
+        Pageable pageable = PageRequest.of(0,12);
+
+        Page<ProductMinDTO> result = productService.findAll(productName, pageable);
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.getSize(), 1);
+        Assertions.assertEquals(result.iterator().next().getName(), productName);
+    }
 }
