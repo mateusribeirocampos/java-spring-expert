b01e176b97fb45444a063161a89818a3cce27605
mateusribeirocampos <mateusrcampos@gmail.com>
2026-02-16 19:09:00 -0300

    Chore: findAll implemented to desafio restassured

--- cap6/desafio-dsmovie-restassured/dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ cap6/desafio-dsmovie-restassured/dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,20 +1,63 @@
 package com.devsuperior.dsmovie.controllers;
 
 import org.json.JSONException;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 public class MovieControllerRA {
+
+	private String movieTitle;
+	private Long existingId, nonExistingId;
+
+	@BeforeEach
+	void setUp() throws JSONException {
+		baseURI = "http://localhost:8080";
+
+		movieTitle = "Matrix";
+
+	}
 	
 	@Test
 	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+		given()
+				.get("/movies")
+				.then()
+				.body("content.title", hasItems("Venom: Tempo de Carnificina",
+						"Star Wars: A Guerra dos Clones",
+						"O Silêncio dos Inocentes"));
 	}
 	
 	@Test
-	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {		
+	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {
+		given()
+				.get("/movies?title={movieTitle}", movieTitle)
+				.then()
+				.statusCode(200)
+				.body("content.id[0]", is(4))
+				.body("content.title[0]", equalTo("Matrix Resurrections"))
+				.body("content.score[0]", is(0.0f))
+				.body("content.count[0]", is(0))
+				.body("content.image[0]", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg"));
 	}
 	
 	@Test
-	public void findByIdShouldReturnMovieWhenIdExists() {		
+	public void findByIdShouldReturnMovieWhenIdExists() {
+		existingId = 11L;
+
+		given()
+				.get("/movies/{id}", existingId)
+				.then()
+				.statusCode(200)
+				.body("id", is(11))
+				.body("title", equalTo("Star Wars: A Guerra dos Clones"))
+				.body("score", is(0.0f))
+				.body("count", is(0))
+				.body("image", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/uK15I3sGd8AudO9z6J6vi0HH1UU.jpg"));
 	}
 	
 	@Test
